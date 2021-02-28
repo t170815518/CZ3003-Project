ff59c2f6d0b6de0f0ccba216742040b7ae4671e3
t170815518 <christang_1023@outlook.com>
2021-02-28 16:04:33 +0800

    Update game logic for single player quiz

--- SinglePlayerQuiz/AnswerField.gd
+++ SinglePlayerQuiz/AnswerField.gd
@@ -9,15 +9,13 @@ var option2
 var option3
 var option4
 
+signal correct_answer 
+signal wrong_answer 
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	update_questions("mcq")
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
 
 
 func update_questions(type):
@@ -25,14 +23,27 @@ func update_questions(type):
 		option1 = Button.new()
 		option1.set_position(Vector2(64, 256))
 		add_child(option1)
+		option1.connect("pressed", self, "_on_Player_press_button", [1])
 		option2 = Button.new()
 		option2.set_position(Vector2(320, 256))
 		add_child(option2)
+		option2.connect("pressed", self, "_on_Player_press_button", [2])
 		option3 = Button.new()
 		option3.set_position(Vector2(574, 256))
 		add_child(option3)
+		option3.connect("pressed", self, "_on_Player_press_button", [3])
 		option4 = Button.new()
 		option4.set_position(Vector2(704, 256))
 		add_child(option4)
+		option4.connect("pressed", self, "_on_Player_press_button", [4])
 	else:
 		pass 
+
+
+# check if the answer is correct for MCQ question 
+func _on_Player_press_button(index):	
+	print("User clicks button", index)
+	if index == 4:  # assume the correct answer is 4 
+		emit_signal("correct_answer")
+	else:
+		emit_signal("wrong_answer")
--- SinglePlayerQuiz/QuizField.gd
+++ SinglePlayerQuiz/QuizField.gd
@@ -8,9 +8,27 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-	
+	get_node("AnswerField").connect("correct_answer", self, "_on_correct_answer")
+	get_node("AnswerField").connect("wrong_answer", self, "_on_wrong_answer")	
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+# when the user gives the correct answer 
+func _on_correct_answer():
+	var enemy 
+	enemy = get_node("Enemy")
+	enemy.hp -= 1
+	if enemy.hp > 0: 
+		get_node("Enemy/HpValue").set_text(str(enemy.hp))
+	else:
+		print("User wins the quiz!")  # player wins the quiz 
+	# refresh the question 
+
+
+func _on_wrong_answer():
+	var player 
+	player = get_node("Player")
+	player.hp -= 1
+	if player.hp > 0: 
+		get_node("Player/HpValue").set_text(str(player.hp))
+	else:
+		print("User loses the quiz!")  # player loses the quiz 
+	# refresh the question 
