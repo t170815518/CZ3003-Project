cdb09451531b8720416d7aacd92e114143cad111
chunfoong <chunfoong95@hotmail.com>
2021-04-15 10:09:02 +0800

    world commit without the if statement

--- world/world.gd
+++ world/world.gd
@@ -29,14 +29,14 @@ func _on_world_3_pressed():
 	Websocket.send({"method":"createRoom", "worldNumber":3}) #send world number to server
 
 	OS.delay_msec(150)
-	if global.roomCreated == true:
-		var root = get_tree().get_root()
-		var next_scnene = load("res://room/PlayerInvites.tscn").instance()
-		root.remove_child(self)
-		OS.delay_msec(50)  # for user response  
-		root.add_child(next_scnene)
-	else:
-		$Popup.popup()
+	#if global.roomCreated == true:
+	var root = get_tree().get_root()
+	var next_scnene = load("res://room/PlayerInvites.tscn").instance()
+	root.remove_child(self)
+	OS.delay_msec(50)  # for user response  
+	root.add_child(next_scnene)
+	#else:
+		#$Popup.popup()
 		
 
 func _on_world_2_pressed():
@@ -44,107 +44,107 @@ func _on_world_2_pressed():
 	Websocket.send({"method":"createRoom", "worldNumber":2}) #send world number to server
 
 	OS.delay_msec(150)
-	if global.roomCreated == true:
-		var root = get_tree().get_root()
-		var next_scnene = load("res://room/PlayerInvites.tscn").instance()
-		root.remove_child(self)
-		OS.delay_msec(50)  # for user response  
-		root.add_child(next_scnene)
-	else:
-		$Popup.popup()
+	#if global.roomCreated == true:
+	var root = get_tree().get_root()
+	var next_scnene = load("res://room/PlayerInvites.tscn").instance()
+	root.remove_child(self)
+	OS.delay_msec(50)  # for user response  
+	root.add_child(next_scnene)
+#	else:
+#		$Popup.popup()
 
 func _on_world_1_pressed():
 	global.selected_world = 1
 	Websocket.send({"method":"createRoom", "worldNumber":1}) #send world number to server
 
 	OS.delay_msec(150)
-	if global.roomCreated == true:
-		var root = get_tree().get_root()
-		var next_scnene = load("res://room/PlayerInvites.tscn").instance()
-		root.remove_child(self)
-		OS.delay_msec(50)  # for user response  
-		root.add_child(next_scnene)
-	else:
-		$Popup.popup()
+#	if global.roomCreated == true:
+	var root = get_tree().get_root()
+	var next_scnene = load("res://room/PlayerInvites.tscn").instance()
+	root.remove_child(self)
+	OS.delay_msec(50)  # for user response  
+	root.add_child(next_scnene)
+	#else:
+	#	$Popup.popup()
 
 func _on_world_4_pressed():
 	global.selected_world = 4
 	Websocket.send({"method":"createRoom", "worldNumber":4}) #send world number to server
 
 	OS.delay_msec(150)
-	if global.roomCreated == true:
-		var root = get_tree().get_root()
-		var next_scnene = load("res://room/PlayerInvites.tscn").instance()
-		root.remove_child(self)
-		OS.delay_msec(50)  # for user response  
-		root.add_child(next_scnene)
-	else:
-		$Popup.popup()
+	#if global.roomCreated == true:
+	var root = get_tree().get_root()
+	var next_scnene = load("res://room/PlayerInvites.tscn").instance()
+	root.remove_child(self)
+	OS.delay_msec(50)  # for user response  
+	root.add_child(next_scnene)
+	#else:
+	#	$Popup.popup()
 
 func _on_world_5_pressed():
 	global.selected_world = 5
 	Websocket.send({"method":"createRoom", "worldNumber":5}) #send world number to server
 	OS.delay_msec(150)
-	if global.roomCreated == true:
-		var root = get_tree().get_root()
-		var next_scnene = load("res://room/PlayerInvites.tscn").instance()
-		root.remove_child(self)
-		OS.delay_msec(50)  # for user response  
-		root.add_child(next_scnene)
-	else:
-		$Popup.popup()
+	#if global.roomCreated == true:
+	var root = get_tree().get_root()
+	var next_scnene = load("res://room/PlayerInvites.tscn").instance()
+	root.remove_child(self)
+	OS.delay_msec(50)  # for user response  
+	root.add_child(next_scnene)
+	#else:
+	#	$Popup.popup()
 
 func _on_world_6_pressed():
 	global.selected_world = 6
 	Websocket.send({"method":"createRoom", "worldNumber":6}) #send world number to server
 
 	OS.delay_msec(150)
-	if global.roomCreated == true:
-		var root = get_tree().get_root()
-		var next_scnene = load("res://room/PlayerInvites.tscn").instance()
-		root.remove_child(self)
-		OS.delay_msec(50)  # for user response  
-		root.add_child(next_scnene)
-	else:
-		$Popup.popup()
+	#if global.roomCreated == true:
+	var root = get_tree().get_root()
+	var next_scnene = load("res://room/PlayerInvites.tscn").instance()
+	root.remove_child(self)
+	OS.delay_msec(50)  # for user response  
+	root.add_child(next_scnene)
+	#else:
+	#	$Popup.popup()
 
 func _on_world_7_pressed():
 	global.selected_world = 7
 	Websocket.send({"method":"createRoom", "worldNumber":7}) #send world number to server
 	OS.delay_msec(150)
-	if global.roomCreated == true:
-		var root = get_tree().get_root()
-		var next_scnene = load("res://room/PlayerInvites.tscn").instance()
-		root.remove_child(self)
-		OS.delay_msec(50)  # for user response  
-		root.add_child(next_scnene)
-	else:
-		$Popup.popup()
+#	if global.roomCreated == true:
+	var root = get_tree().get_root()
+	var next_scnene = load("res://room/PlayerInvites.tscn").instance()
+	root.remove_child(self)
+	OS.delay_msec(50)  # for user response  
+	root.add_child(next_scnene)
+	#else:
+	#	$Popup.popup()
 
 func _on_world_8_pressed():
 	global.selected_world = 8
 	Websocket.send({"method":"createRoom", "worldNumber":8}) #send world number to server
 	OS.delay_msec(150)
-	if global.roomCreated == true:
-		var root = get_tree().get_root()
-		var next_scnene = load("res://room/PlayerInvites.tscn").instance()
-		root.remove_child(self)
-		OS.delay_msec(50)  # for user response  
-		root.add_child(next_scnene)
-	else:
-		$Popup.popup()
-
+	#if global.roomCreated == true:
+	var root = get_tree().get_root()
+	var next_scnene = load("res://room/PlayerInvites.tscn").instance()
+	root.remove_child(self)
+	OS.delay_msec(50)  # for user response  
+	root.add_child(next_scnene)
+	#else:
+	#	$Popup.popup()
+#
 
 func _on_world_9_pressed():
 	global.selected_world = 9
 	Websocket.send({"method":"createRoom", "worldNumber":9}) #send world number to server
 
 	OS.delay_msec(150)
-	if global.roomCreated == true:
-		var root = get_tree().get_root()
-		var next_scnene = load("res://room/PlayerInvites.tscn").instance()
-		root.remove_child(self)
-		OS.delay_msec(50)  # for user response  
-		root.add_child(next_scnene)
-	else:
-		$Popup.popup()
+	#if global.roomCreated == true:
+	var root = get_tree().get_root()
+	var next_scnene = load("res://room/PlayerInvites.tscn").instance()
+	root.remove_child(self)
+	OS.delay_msec(50)  # for user response  
+	root.add_child(next_scnene)
+	#else:
+	#	$Popup.popup()
